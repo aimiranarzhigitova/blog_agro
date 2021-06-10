feb104ae7fe46d0fc32695ea91a3dec938e82418
aimiranarzhigitova <aymira.narzhigitova@gmail.com>
2021-06-10 17:13:58 +0600

    =

--- blog_agro/settings.py
+++ blog_agro/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'blog_agro.wsgi.application'
 DATABASES = {
 'default': {
 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'ENGINE': 'django.db.backends.postgresql',
 'NAME': 'agro',
 'USER': 'admin',
 'PASSWORD': 'admin',
