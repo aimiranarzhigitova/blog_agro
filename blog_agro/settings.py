8e401f7fa7660cec362cd7e71f0ed2170d0dd049
aimiranarzhigitova <aymira.narzhigitova@gmail.com>
2021-06-10 16:51:03 +0600

    -

--- blog_agro/settings.py
+++ blog_agro/settings.py
@@ -140,10 +140,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
