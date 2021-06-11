0d4461652bf3a7fc58ce35a75f35bac509efb78b
aimiranarzhigitova <aymira.narzhigitova@gmail.com>
2021-06-11 14:12:12 +0600

    =

--- blog_agro/settings.py
+++ blog_agro/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 1,
+    'PAGE_SIZE': 10,
 }
 
 AUTH_USER_MODEL = 'agro_user.AgroUser'
